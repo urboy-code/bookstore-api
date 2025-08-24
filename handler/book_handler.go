911e7495df4bbda50ef1904b5dd5a0e4853c1315
urboy-code <42699288+urboy-code@users.noreply.github.com>
2025-08-24 18:27:14 +0700

    feat(handler): Add GetBookByID Handler

--- handler/book_handler.go
+++ handler/book_handler.go
@@ -3,7 +3,9 @@ package handler
 import (
 	"bookstore-api/model"
 	"bookstore-api/repository"
+	"database/sql"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -43,4 +45,49 @@ func (h *BookHandler) CreateBookHandler(c *gin.Context){
 	input.ID = bookID
 
 	c.JSON(http.StatusCreated, input)
+
+}
+
+// GetBooksHandler adalah fungsi untuk menangani permintaan mendapatkan semua daftar buku
+// @Summary Get all books
+// @Description Get all books from the database
+// @Tags books
+// @Accept json
+// @Produce json
+// @Success 200 {array} model.Book
+// @Failure 500 {object} gin.H{"error": "Internal Server Error"}
+// @Router /books [get]
+func (h *BookHandler) GetBooksHandler(c *gin.Context){
+	books, err := h.repo.GetBooks()
+	if err != nil{
+		c.JSON(http.StatusInternalServerError, gin.H{"error" : "Failed to retrieve books"})
+		return
+	}
+
+	c.JSON(http.StatusOK, books)
+}	
+
+// GetBookByIDHandler adalah fungsi untuk menangani permintaan mendapatkan buku berdasarkan ID
+
+func (h *BookHandler) GetBookByIDHandler(c *gin.Context){
+	idStr := c.Param("id")
+
+	id, err := strconv.Atoi(idStr)
+	if (err != nil){
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid book ID"})
+		return
+	}
+
+	book, err := h.repo.GetBookByID(id)
+	if err != nil{
+		if err == sql.ErrNoRows{
+			c.JSON(http.StatusNotFound, gin.H{"error": "Book not found"})
+			return
+		}
+
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to retrieve book"})
+		return
+	}
+
+	c.JSON(http.StatusOK, book)
 }
