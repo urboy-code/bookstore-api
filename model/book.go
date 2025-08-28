ce4e5e6b06555e04a295dc3006b8029bcb63f427
urboy-code <42699288+urboy-code@users.noreply.github.com>
2025-08-29 00:02:02 +0700

    feat(model): Implement validating input

--- model/book.go
+++ model/book.go
@@ -2,7 +2,7 @@ package model
 
 type Book struct {
 	ID          int    `json:"id"`
-	Title       string `json:"title"`
-	Author      string `json:"author"`
+	Title       string `json:"title" binding:"required"`
+	Author      string `json:"author" binding:"required"`
 	Description string `json:"description"`
 }
