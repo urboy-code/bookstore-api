5ac3bf3424f714304727d7bf27664b1882422a6f
urboy-code <42699288+urboy-code@users.noreply.github.com>
2025-09-01 13:26:54 +0700

    feat(model): Create user table

--- model/user.go
+++ model/user.go
@@ -1,9 +1,9 @@
 package model
 
 type User struct {
-	ID			 int64  `json:"id"`
-	Name		 string `json:"name" binding:"required"` 
-	Email		 string `json:"email" binding:"required,email"`
-	Password	 string `json:"password,omitempty" binding:"required,min=6"`
-	PasswordHash string `json:"-"`
+	ID			 			int64  `json:"id"`
+	Name		 			string `json:"name" binding:"required"` 
+	Email		 			string `json:"email" binding:"required,email"`
+	Password			string `json:"password,omitempty" binding:"required,min=6"`
+	PasswordHash 	string `json:"-"`
 }
