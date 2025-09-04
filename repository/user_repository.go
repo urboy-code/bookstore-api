05bd34e6a945b906b7f17e9a07e91d4270d809f7
urboy-code <42699288+urboy-code@users.noreply.github.com>
2025-09-04 12:08:54 +0700

    feat(password): Implement hashing function for password

---
+++ repository/user_repository.go
@@ -0,0 +1,55 @@
+package repository
+
+import (
+	"database/sql"
+	"bookstore-api/model"
+	"errors"
+	"strings"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+// define custom errors for user
+var ErrMailExists = errors.New("email already exists")
+
+type UserRepository struct {
+	db *sql.DB
+}
+
+// CreateUser for hashing password and storing user in db
+func (r *UserRepository) CreateUser(user *model.User) error{
+	// hash the password
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+	if err != nil{
+		return err
+	}
+
+	var userID int
+
+	// Save the user to the database with hashed password
+	query := `INSERT INTO users (name, email, password_hash) values ($1, $2, $3) RETURNING id`
+	err = r.db.QueryRow(query, user.Name, user.Email, string(hashedPassword)).Scan(&userID)
+	if err != nil {
+		// Check error if any existing email (violates unique constraint)
+		if strings.Contains(err.Error(), "unique constraint"){
+			return ErrMailExists
+		}
+		return err
+	}
+	return nil
+}
+
+// GetUserByEmail fetch user by email
+func (r *UserRepository) GetUserByEmail(email string) (model.User, error){
+	var user model.User
+	query := `SELECT id, name, email, password_hash FROM users WHERE email=$1`
+
+	err := r.db.QueryRow(query, email).Scan(&user.ID, &user.Name, &user.Email, &user.PasswordHash)
+	if err != nil{
+		if err == sql.ErrNoRows{
+			return user, ErrBookNotFound
+		}
+		return user, err
+	}
+	return user, nil
+}
