b6e1315f082863f5b8e7553d54f442a875c554ad
urboy-code <42699288+urboy-code@users.noreply.github.com>
2025-09-07 17:11:02 +0700

    feat(login): Create LoginUserHandler and JWT token generation

--- handler/user_handler.go
+++ handler/user_handler.go
@@ -4,7 +4,10 @@ import (
 	"bookstore-api/model"
 	"bookstore-api/repository"
 	"net/http"
+	"os"
+	"time"
 
+	"github.com/golang-jwt/jwt/v5"
 	"github.com/gin-gonic/gin"
 )
 
@@ -53,3 +56,51 @@ func (h *UserHandler) RegisterUserHandler(c *gin.Context){
 	})
 }
 
+// LoginUserHandler handles user login and JWT token generation
+func (h *UserHandler) LoginUserHandler(c *gin.Context){
+	var input struct{
+		Email		string `json:"email" binding:"required,email"`
+		Password	string `json:"password" binding:"required,min=6"`
+	}
+
+	if err := c.ShouldBindJSON(&input); err != nil{
+		c.JSON(http.StatusBadRequest, model.AppError{
+			Code: http.StatusBadRequest,
+			Message: "Invalid input: " + err.Error(),
+		})
+		return
+	}
+
+	// Verify user credentials to repository
+	user, err := h.repo.GetUserByEmail(input.Email)
+	if err != nil{
+		c.JSON(http.StatusUnauthorized, model.AppError{
+			Code: http.StatusUnauthorized,
+			Message: "Invalid email or password",
+		})
+		return
+	}
+
+	// Generate JWT TOKEN
+	// Create "claims" which include data we want to include in the token
+	claims := jwt.MapClaims{
+		"user_id" : user.ID,
+		"email" : user.Email,
+		"exp" : time.Now().Add(time.Hour * 24).Unix(), // Token expires in 24 hours
+	}
+
+	// Create the token using the claims
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	// Sign the token with a secret key
+	tokenString, err := token.SignedString([]byte(os.Getenv("JWT_SECRET_KEY")))
+	if err != nil{
+		ErrorHandler(c, err)
+		return
+	}
+
+	// Send the token in response
+	c.JSON(http.StatusOK, gin.H{
+		"token": tokenString,
+	})
+}
